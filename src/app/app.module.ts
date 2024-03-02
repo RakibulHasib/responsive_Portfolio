d4bb895d511c4c4abf7a51b079462d44cc41901e
Rakibul Hasib <rakimhasib786@gmail.com>
2024-03-02 23:22:53 +0600

    initial-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+import { HomePageComponent } from './content/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './content/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
